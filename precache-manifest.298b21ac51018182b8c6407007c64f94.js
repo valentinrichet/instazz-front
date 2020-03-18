82ef8623796a9788e5f471074dbd65a856787923
Dark3DVFX <valentin.richet@gmail.com>
2020-03-18 20:05:57 +0100

    Updates

---
+++ precache-manifest.298b21ac51018182b8c6407007c64f94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007f9fd9777d1a328b64102a26ed54f5",
+    "url": "/instazz-front/index.html"
+  },
+  {
+    "revision": "7cc1086ba2f8f01e1d4e",
+    "url": "/instazz-front/static/css/main.e1b2a460.chunk.css"
+  },
+  {
+    "revision": "308289c10fe227a7d41c",
+    "url": "/instazz-front/static/js/2.927da64d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/instazz-front/static/js/2.927da64d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc1086ba2f8f01e1d4e",
+    "url": "/instazz-front/static/js/main.ea65cbfb.chunk.js"
+  },
+  {
+    "revision": "5e5e7a6753782ecc144a",
+    "url": "/instazz-front/static/js/runtime-main.b3e2e24d.js"
+  },
+  {
+    "revision": "ba301dbfd7ba6859768a6fbe96f48462",
+    "url": "/instazz-front/static/media/fondEcran.ba301dbf.png"
+  }
+]);
